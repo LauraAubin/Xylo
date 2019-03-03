9723fcfe297a22fe1137c79b0107909b8283b965
Laura Aubin <laura.aubin@shopify.com>
2019-03-02 23:35:55 -0500

    Create a server with a writeFile post response

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.post("/writeFile", (request, response) => {
+  const { file, contents } = request.body;
+
+  response.send({ fileContents: writeFile(file, contents) });
+});
+
+function writeFile(file, contents) {
+  return fs.writeFileSync(file, contents, "utf8");
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
