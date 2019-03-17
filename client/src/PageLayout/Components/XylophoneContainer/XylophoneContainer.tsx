889e1651d8920de2f2307c682e7be62d681cd593
Laura Aubin <laura.aubin@shopify.com>
2019-03-17 16:46:35 -0400

    Create a play password button on the container

--- client/src/PageLayout/Components/XylophoneContainer/XylophoneContainer.tsx
+++ client/src/PageLayout/Components/XylophoneContainer/XylophoneContainer.tsx
@@ -5,28 +5,38 @@ import { Button } from "@shopify/polaris";
 import autobind from "autobind-decorator";
 import Xylophone from "./components/Xylophone";
 
+import "./XylophoneContainer.scss";
+
+interface Props {
+  numberOfKeys: number;
+  generatedPassword: number[];
+}
+
 interface State {
   repeatPasswordVisualization: number;
 }
 
-export default class XylophoneContainer extends React.Component<{}, State> {
-  constructor(state: State) {
-    super(state);
+export default class XylophoneContainer extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
 
     this.state = { repeatPasswordVisualization: 0 };
   }
 
   public render() {
+    const { numberOfKeys, generatedPassword } = this.props;
     const { repeatPasswordVisualization } = this.state;
 
     return (
-      <div>
-        <Button onClick={this.visualizePassword}>Play password</Button>
+      <div className="Center">
         <Xylophone
-          numberOfKeys={5}
-          generatedPassword={[1, 2, 2]}
+          numberOfKeys={numberOfKeys}
+          generatedPassword={generatedPassword}
           repeatPasswordVisualization={repeatPasswordVisualization}
         />
+        <Button plain onClick={this.visualizePassword}>
+          Play password
+        </Button>
       </div>
     );
   }
