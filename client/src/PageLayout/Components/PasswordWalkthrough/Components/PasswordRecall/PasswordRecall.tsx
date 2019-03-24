f2ad555824176cce34bb360e1d1c8c8bbf7258ab
Laura Aubin <laura.aubin@shopify.com>
2019-03-24 16:31:26 -0400

    Create PasswordRecall component

--- client/src/PageLayout/Components/PasswordWalkthrough/Components/PasswordRecall/PasswordRecall.tsx
+++ client/src/PageLayout/Components/PasswordWalkthrough/Components/PasswordRecall/PasswordRecall.tsx
@@ -1,11 +1,58 @@
 import * as React from 'react';
 
-export default class PasswordRecall extends React.Component {
+import XylophoneContainer from "../../../XylophoneContainer";
+
+import { Button, Modal } from "@shopify/polaris";
+
+enum Type {
+  creation,
+  recall
+}
+
+interface Props {
+  showModal: boolean;
+  passwordOptions: number;
+  password: number[];
+  closeModal(): void;
+  handleModal(): void;
+}
+
+export default class PasswordRecall extends React.Component<Props> {
   public render() {
+    const {
+      showModal,
+      passwordOptions,
+      password,
+      closeModal,
+      handleModal
+    } = this.props;
+
+    const modalMarkup = (
+      <Modal
+        large
+        title="Try to recall the password for x"
+        open={showModal}
+        onClose={closeModal}
+        primaryAction={{
+          content: "Got it",
+          onAction: closeModal
+        }}
+      >
+        <Modal.Section>
+          <XylophoneContainer
+            type={Type.recall}
+            numberOfKeys={passwordOptions}
+            password={password}
+          />
+        </Modal.Section>
+      </Modal>
+    );
+
     return (
-      <div>
-        Here I test your ability to remember the password
-      </div>
+      <>
+        <Button onClick={handleModal}>Remember password</Button>
+        {modalMarkup}
+      </>
     );
   }
 }
