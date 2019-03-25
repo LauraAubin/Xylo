c5c2f5a090d43203f14c691c40f42d5cc811736d
Laura Aubin <laura.aubin@shopify.com>
2019-03-24 22:06:49 -0400

    Create named stack

--- client/src/PageLayout/components/PasswordWalkthrough/components/PasswordCreation/PasswordCreation.tsx
+++ client/src/PageLayout/components/PasswordWalkthrough/components/PasswordCreation/PasswordCreation.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 
 import XylophoneContainer from "../../../XylophoneContainer";
 
-import { Button, Modal } from "@shopify/polaris";
+import { Icon, Modal, Stack, TextStyle } from "@shopify/polaris";
+
+import "./PasswordCreation.scss";
 
 enum Type {
   creation,
@@ -13,6 +15,7 @@ interface Props {
   showModal: boolean;
   passwordOptions: number;
   generatedPassword: number[];
+  step: number;
   closeModal(): void;
   handleModal(): void;
 }
@@ -23,6 +26,7 @@ export default class PasswordCreation extends React.Component<Props> {
       showModal,
       passwordOptions,
       generatedPassword,
+      step,
       closeModal,
       handleModal
     } = this.props;
@@ -48,9 +52,46 @@ export default class PasswordCreation extends React.Component<Props> {
       </Modal>
     );
 
+    const pageMarkup = [
+      { type: "Shopping", color: "blue", icon: "products" },
+      { type: "Home", color: "green", icon: "home" },
+      { type: "Phone", color: "red", icon: "notification" }
+    ].map((item, index) => (
+      <div className="StackElement">
+        <Stack alignment="center">
+          <Stack.Item>
+            <div className={`Circle Circle--${item.color}`}>
+              <Icon
+                source={
+                  item.icon === "products"
+                    ? "products"
+                    : item.icon === "home"
+                    ? "home"
+                    : "notification"
+                }
+                color="white"
+              />
+            </div>
+          </Stack.Item>
+          <Stack.Item fill>
+            <TextStyle variation="subdued">{item.type}</TextStyle>
+          </Stack.Item>
+          <Stack.Item>
+            {index === step && (
+              <ul className="ArrowButton">
+                <li>
+                  <a onClick={handleModal}>Create password</a>
+                </li>
+              </ul>
+            )}
+          </Stack.Item>
+        </Stack>
+      </div>
+    ));
+
     return (
       <>
-        <Button onClick={handleModal}>Create password</Button>
+        {pageMarkup}
         {modalMarkup}
       </>
     );
